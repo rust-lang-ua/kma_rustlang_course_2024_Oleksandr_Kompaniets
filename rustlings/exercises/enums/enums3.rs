de18506307f19114ae5c2408c93b3421643c6452
Kasgor <shadow.crst@gmail.com>
2024-10-16 14:43:36 +0300

    Vec

--- rustlings/exercises/enums/enums3.rs
+++ rustlings/exercises/enums/enums3.rs
@@ -1,9 +1,13 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
+// I AM DONE
 
 enum Message {
+    Move(Point),
+    Echo(String),
+    ChangeColor((u8, u8, u8)),
+    Quit,
     // TODO: implement the message variant types based on their usage below
 }
 
@@ -36,6 +40,13 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        match message {
+            Message::Move(p) => self.move_position(p),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor((r, g, b)) => self.change_color((r, g, b)),
+           
+            Message::Quit => self.quit()
+        }
         // TODO: create a match expression to process the different message variants
     }
 }
